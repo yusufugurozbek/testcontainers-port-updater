a017962803bf62804926a5d5cf246a7403c80c14
Yusuf Ugur ÖZBEK <yusuf.ugur.oezbek@telekom.de>
2021-08-31 12:44:29 +0200

    Fix 2 deprecated API usages

--- src/main/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/TpuNotifier.kt
+++ src/main/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/TpuNotifier.kt
@@ -1,22 +1,15 @@
 package com.github.yusufugurozbek.testcontainers.port.updater
 
-import com.intellij.notification.NotificationDisplayType
-import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
-
-private val NOTIFICATION_GROUP =
-    NotificationGroup(TpuBundle.message("name"), NotificationDisplayType.BALLOON, true)
 
 class TpuNotifier private constructor() {
     companion object {
         fun notify(project: Project, message: String) {
-            val notification = NOTIFICATION_GROUP.createNotification(
-                TpuBundle.message("name"),
-                message,
-                NotificationType.INFORMATION
-            )
-            notification.notify(project)
+            NotificationGroupManager.getInstance().getNotificationGroup(TpuBundle.message("name"))
+                .createNotification(message, NotificationType.INFORMATION)
+                .notify(project)
         }
     }
 }
