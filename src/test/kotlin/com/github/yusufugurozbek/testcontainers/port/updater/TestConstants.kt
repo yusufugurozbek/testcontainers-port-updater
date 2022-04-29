d253609b24f61828d7ef7aa2a5c59557396312bf
Yusuf Ugur ÖZBEK <yusufugurozbek@hotmail.com>
2022-04-29 19:46:56 +0200

    Release 0.1.2 :bookmark:

--- src/test/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/TestConstants.kt
+++ src/test/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/TestConstants.kt
@@ -4,9 +4,9 @@ object TestConstants {
     const val DATASOURCE_URL_1 = "jdbc:postgresql://localhost:55001/test"
     const val DATASOURCE_URL_2 = "jdbc:postgresql://localhost:55001/test?loggerLevel=OFF"
     const val LOG_ENTRY_1 = "2021-07-16 14:05:56.360  INFO [my-application,,] 33702 --- [" +
-            "           main] o.f.c.i.database.base.DatabaseType       : " +
-            "Database: jdbc:postgresql://localhost:55001/test (PostgreSQL 10.17)"
+        "           main] o.f.c.i.database.base.DatabaseType       : " +
+        "Database: jdbc:postgresql://localhost:55001/test (PostgreSQL 10.17)"
     const val LOG_ENTRY_2 = "2021-07-16 14:05:56.360  INFO [my-application,,] 33702 --- [" +
-            "           main] o.f.c.i.database.base.DatabaseType       : " +
-            "Database: jdbc:postgresql://localhost:55001/test?loggerLevel=OFF"
+        "           main] o.f.c.i.database.base.DatabaseType       : " +
+        "Database: jdbc:postgresql://localhost:55001/test?loggerLevel=OFF"
 }
