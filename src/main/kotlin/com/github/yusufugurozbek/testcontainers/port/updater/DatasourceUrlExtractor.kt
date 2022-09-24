ec12591b6f8553566545dc76155538e42c946991
ManolisPapd <valkon.gr@gmail.com>
2022-09-24 20:28:58 +0300

    Modified regex to handle also MSSQL Server URL

--- src/main/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/DatasourceUrlExtractor.kt
+++ src/main/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/DatasourceUrlExtractor.kt
@@ -2,7 +2,7 @@ package com.github.yusufugurozbek.testcontainers.port.updater
 
 class DatasourceUrlExtractor {
 
-    private val regex: Regex = "Database: (.*?)(?!\\S)".toRegex()
+    private val regex: Regex = "Database: ((.*?)((?!\\S)(\\s.*\\w.*;|)))".toRegex()
 
     internal fun extract(from: String): String? {
         val find = regex.find(from)
--- src/test/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/DatasourceUrlExtractorTest.kt
+++ src/test/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/DatasourceUrlExtractorTest.kt
@@ -2,8 +2,10 @@ package com.github.yusufugurozbek.testcontainers.port.updater
 
 import com.github.yusufugurozbek.testcontainers.port.updater.TestConstants.DATASOURCE_URL_1
 import com.github.yusufugurozbek.testcontainers.port.updater.TestConstants.DATASOURCE_URL_2
+import com.github.yusufugurozbek.testcontainers.port.updater.TestConstants.DATASOURCE_URL_MSSQL_SERVER
 import com.github.yusufugurozbek.testcontainers.port.updater.TestConstants.LOG_ENTRY_1
 import com.github.yusufugurozbek.testcontainers.port.updater.TestConstants.LOG_ENTRY_2
+import com.github.yusufugurozbek.testcontainers.port.updater.TestConstants.LOG_ENTRY_MSSQL_SERVER
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -24,5 +26,10 @@ internal class DatasourceUrlExtractorTest {
     @Test
     fun `extractDataSourceUrl returns null if not found`() {
         assertEquals(sut.extract("someGibberish"), null)
+    }
+
+    @Test
+    fun `extractDataSourceUrl successfully extracts mssql server url`() {
+        assertEquals(sut.extract(LOG_ENTRY_MSSQL_SERVER), DATASOURCE_URL_MSSQL_SERVER)
     }
 }
