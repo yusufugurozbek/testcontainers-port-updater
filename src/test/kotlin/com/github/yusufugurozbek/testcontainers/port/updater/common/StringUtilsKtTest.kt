55b49ed4c7732cc19fb5c63c45fcc152e7c24862
Yusuf Ugur ÖZBEK <yusufugurozbek@hotmail.com>
2022-12-09 15:55:00 +0100

    Migrate latest changes of intellij-platform-plugin-template

--- src/test/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/common/StringUtilsKtTest.kt
+++ src/test/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/common/StringUtilsKtTest.kt
@@ -1,27 +1,22 @@
 package com.github.yusufugurozbek.testcontainers.port.updater.common
 
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
-internal class StringUtilsKtTest {
+internal class StringUtilsKtTest : BasePlatformTestCase() {
 
-    @Test
-    fun `equalsIgnoringPort successfully detects equality`() {
+    fun `test equalsIgnoringPort successfully detects equality`() {
         val jdbcStringA = "jdbc:postgresql://localhost:55001/test"
         val jdbcStringB = "jdbc:postgresql://localhost:55035/test"
         assertTrue(jdbcStringA.equalsIgnoringPort(jdbcStringB))
     }
 
-    @Test
-    fun `equalsIgnoringPort successfully detects equality with IP addresses as hostnames`() {
+    fun `test equalsIgnoringPort successfully detects equality with IP addresses as hostnames`() {
         val jdbcStringA = "jdbc:postgresql://127.0.0.1:55001/test"
         val jdbcStringB = "jdbc:postgresql://127.0.0.1:55002/test"
         assertTrue(jdbcStringA.equalsIgnoringPort(jdbcStringB))
     }
 
-    @Test
-    fun `equalsIgnoringPort successfully detects inequality`() {
+    fun `test equalsIgnoringPort successfully detects inequality`() {
         val jdbcStringA = "jdbc:postgresql://localhost:55001/test"
         val jdbcStringB = "jdbc:sqlserver://localhost:55001/test"
         assertFalse(jdbcStringA.equalsIgnoringPort(jdbcStringB))
