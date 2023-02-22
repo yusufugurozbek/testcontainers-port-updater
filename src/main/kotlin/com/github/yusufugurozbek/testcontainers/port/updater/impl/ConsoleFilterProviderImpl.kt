a58ab036a78d4859c87ee142180f7f24fae3e199
Yusuf Ugur ÖZBEK <yusufugurozbek@hotmail.com>
2023-02-22 18:31:53 +0100

    refactor!: introduce DataSourceUrl data class
    - move retrieving local data sources to ConsoleFilterProviderImpl,
    that will improve the performance

--- src/main/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/impl/ConsoleFilterProviderImpl.kt
+++ src/main/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/impl/ConsoleFilterProviderImpl.kt
@@ -1,11 +1,17 @@
 package com.github.yusufugurozbek.testcontainers.port.updater.impl
 
+import com.intellij.database.dataSource.LocalDataSource
+import com.intellij.database.psi.DbPsiFacade
+import com.intellij.database.util.DbImplUtil
 import com.intellij.execution.filters.ConsoleFilterProvider
 import com.intellij.execution.filters.Filter
 import com.intellij.openapi.project.Project
 
 class ConsoleFilterProviderImpl : ConsoleFilterProvider {
     override fun getDefaultFilters(project: Project): Array<Filter> {
-        return arrayOf(DataSourceFilter(project))
+        val dataSources: List<LocalDataSource> = DbPsiFacade.getInstance(project).dataSources
+            .mapNotNull { DbImplUtil.getMaybeLocalDataSource(it) }
+
+        return arrayOf(DataSourceFilter(project, dataSources))
     }
 }
