6004dae8e434523a63995d67687c41916275e31e
Yusuf Ugur ÖZBEK <yusufugurozbek@hotmail.com>
2023-08-18 19:28:44 +0200

    refactor: switch TpuSettingsState to a light service
    https://plugins.jetbrains.com/docs/intellij/plugin-services.html?from=jetbrains.org#light-services

--- src/main/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/settings/TpuSettingsState.kt
+++ src/main/kotlin/com/github/yusufugurozbek/testcontainers/port/updater/settings/TpuSettingsState.kt
@@ -1,6 +1,7 @@
 package com.github.yusufugurozbek.testcontainers.port.updater.settings
 
 import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.util.xmlb.XmlSerializerUtil
@@ -9,6 +10,7 @@ import com.intellij.util.xmlb.XmlSerializerUtil
     name = "com.github.yusufugurozbek.testcontainers.port.updater.settings.TpuSettingsState",
     storages = [Storage("TestcontainersPortUpdaterPlugin.xml")]
 )
+@Service(Service.Level.PROJECT)
 class TpuSettingsState : PersistentStateComponent<TpuSettingsState?> {
     var isNotificationsEnabled: Boolean = true
     var logEntryPrefix: String = "Database:"
